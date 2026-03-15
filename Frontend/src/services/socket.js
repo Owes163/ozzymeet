f3b0fe3ac8c9c97e857a6eea8a74c0444d4012ba
Owes Shaikh <ozzyshaikh163@gmail.com>
2026-03-15 13:04:41 +0530

    fix cors

--- Frontend/src/services/socket.js
+++ Frontend/src/services/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const BACKEND_URL = "https://ozzymeet-backend.onrender.com";
 
 export const socket = io(BACKEND_URL, {
-  transports: ["websocket"]
+  transports: ["websocket"],
 });
 
 export const API_URL = BACKEND_URL;
